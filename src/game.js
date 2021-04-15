f8aaa5b8bd74a69e431d5be7b16053ca6b77246f
Enzo Grzeskiewicz <enzo.grzeskiewicz@ynov.com>
2021-04-15 23:16:49 +0200

    Added pause state and pause menu

--- src/game.js
+++ src/game.js
@@ -3,6 +3,17 @@ import Joueur from "./joueur.js"
 import Ennemie from "./ennemie.js"
 import {buildLevel,level_Test} from "./levels.js"
 
+
+const GAMESTATE = {
+    PAUSED: 0,
+    RUNNING: 1,
+    MENU: 2,
+    GAMEOVER: 3,
+}
+
+
+
+
 export default class Game {
     constructor(gameWidth,gameHeight){
         this.gameWidth=gameWidth
@@ -10,22 +21,51 @@ export default class Game {
         
     }
     init(){
+
+        this.GAMESTATE = GAMESTATE.RUNNING //On indique que le jeu est "en cours".
+
         this.joueur= new Joueur(this)
 
         this.ennemies = buildLevel(this.gameHeight,level_Test) //   construit le level
         
-        new InputHandler(this.joueur,this.ctx);
+        new InputHandler(this.joueur,this);
     }
     update(deltatime){
+        if(this.GAMESTATE == GAMESTATE.PAUSED) return // Ne pas update si le jeu est en pause
         this.joueur.update(deltatime)
+        
     }
     draw(ctx){
         this.joueur.dessin(ctx)
         
+        // Fond bleu pour pouvoir distinguer le menu pause - temporaire
+        ctx.rect(0,0,this.gameWidth,this.gameHeight)
+        ctx.fillStyle = "rgba(0,200,255,0.5)"
+        ctx.fill()
+
+
+        // Fond noir transparent quand on appuie sur ESC/jeu en pause
+        if (this.GAMESTATE == GAMESTATE.PAUSED) {  
+            ctx.rect(0, 0, this.gameWidth, this.gameHeight)
+            ctx.fillStyle = "rgba(0,0,0,0.5)"
+            ctx.fill()
+        }
+
         // affiche chaque ennemie dans
         this.ennemies.forEach(e=>{
             e.draw(ctx)
         })
         
+    }
+    
+    togglePause(){
+        //Si le jeu est en cours; le mettre en pause.
+        //Sinon, le reprendre.
+        if(this.GAMESTATE == GAMESTATE.RUNNING){
+            this.GAMESTATE = GAMESTATE.PAUSED
+        }else{
+            this.GAMESTATE = GAMESTATE.RUNNING
+        }
+
     }
 }
