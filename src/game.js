6bdffedde37fc58fb2b6b29f131ea0c05446e2dc
Enzo Grzeskiewicz <enzo.grzeskiewicz@ynov.com>
2021-04-23 16:37:29 +0200

    0.08

--- src/game.js
+++ src/game.js
@@ -1,45 +1,43 @@
 import InputHandler from "./input.js"
 import Joueur from "./joueur.js"
-import {Level , level_Test} from "./levels.js"
-import {collisionDetection} from "./collision.js"
+import { Level, level_Test } from "./levels.js"
+import { collisionDetection } from "./collision.js"
 
 const GAMESTATE = {
     PAUSED: 0,
     RUNNING: 1,
     MENU: 2,
     GAMEOVER: 3,
+    WINNER: 4,
 }
 
 export default class Game {
-    constructor(gameWidth,gameHeight){
-        this.gameWidth=gameWidth
-        this.gameHeight=gameHeight
+    constructor(gameWidth, gameHeight) {
+        this.gameWidth = gameWidth
+        this.gameHeight = gameHeight
         this.terminer = false
+        this.GAMESTATE = GAMESTATE.MENU 
+        this.joueur = new Joueur(this)
+        this.level = new Level(this, level_Test)
+        new InputHandler(this.joueur, this);
     }
-    init(){  
-
-        this.GAMESTATE = GAMESTATE.RUNNING //On indique que le jeu est "en cours".
-
-        this.joueur= new Joueur(this)
-        this.level = new Level(this,level_Test)
-        
-        new InputHandler(this.joueur,this);
+    init() {
+        this.GAMESTATE = GAMESTATE.RUNNING
     }
-    update(deltatime){
-        if(this.GAMESTATE == GAMESTATE.PAUSED) return // Ne pas update si le jeu est en pause
-        
-        collisionDetection(this.joueur,this.level,this.terminer)
+    update(deltatime) {
+        if (this.GAMESTATE === GAMESTATE.PAUSED || this.GAMESTATE === GAMESTATE.MENU || this.GAMESTATE === GAMESTATE.GAMEOVER) return // Ne pas update si le jeu n'est pas en cours
+
+        collisionDetection(this.joueur, this.level, this.terminer)
 
         this.joueur.update(deltatime)
         this.level.update(deltatime)
+
     }
-    draw(ctx){
+    draw(ctx) {
         // affiche chaque alien dans le ctx
-        this.level.draw(ctx)
-        this.joueur.dessin(ctx)
-        
+
         // Fond noir transparent quand on appuie sur ESC/jeu en pause
-        if (this.GAMESTATE == GAMESTATE.PAUSED) {  
+        if (this.GAMESTATE == GAMESTATE.PAUSED) {
             ctx.rect(0, 0, this.gameWidth, this.gameHeight)
             ctx.fillStyle = "rgba(0,0,0,0.5)"
             ctx.fill()
@@ -47,6 +45,7 @@ export default class Game {
             ctx.fillStyle = "white";
             ctx.textAlign = "center";
             ctx.fillText("Paused", this.gameWidth / 2, this.gameHeight / 2);
+
         }
         // Fond noir + GAMEOVER si plus de vies
         if (this.GAMESTATE == GAMESTATE.GAMEOVER) {
@@ -57,10 +56,31 @@ export default class Game {
             ctx.fillStyle = "white";
             ctx.textAlign = "center";
             ctx.fillText("GAME OVER", this.gameWidth / 2, this.gameHeight / 2);
+        }else if (this.GAMESTATE === GAMESTATE.MENU) {
+            ctx.rect(0, 0, this.gameWidth, this.gameHeight);
+            ctx.fillStyle = "rgba(0,0,0,1)";
+            ctx.fill();
+
+            ctx.font = "30px Arial";
+            ctx.fillStyle = "white";
+            ctx.textAlign = "center";
+            if (~~(0.5 + Date.now() / 500) % 2) {
+                ctx.fillText(
+                    "Press ENTER To Start",
+                    this.gameWidth / 2,
+                    this.gameHeight / 2
+                );
+              }
+            
+        } else {
+            this.level.draw(ctx)
+            this.joueur.dessin(ctx)
         }
 
+
+
     }
-    
+
     escapeActions() {
         //Si le jeu est en cours; le mettre en pause.
         //Sinon, le reprendre.
@@ -70,13 +90,14 @@ export default class Game {
         } else if (this.GAMESTATE == GAMESTATE.PAUSED) {
             this.GAMESTATE = GAMESTATE.RUNNING
         } else if (this.GAMESTATE == GAMESTATE.GAMEOVER) {
-            //retour au main menu a faire
+            this.GAMESTATE = GAMESTATE.MENU
         }
 
     }
     enterActions() {
         //Si le jeu est en game over, relancer une partie en appuyant sur entrée
-        if (this.GAMESTATE == GAMESTATE.GAMEOVER) {
+        //Si le jeu est au menu principal, on le lance.
+        if (this.GAMESTATE == GAMESTATE.GAMEOVER || this.GAMESTATE == GAMESTATE.MENU ) {
             this.init()
         }
     }
--- src/index.js
+++ src/index.js
@@ -8,15 +8,14 @@ const game_width = 800;
 const game_height = 600;
 
 let game = new Game(game_width,game_height)
-game.init()
+
 let lastime =0;
 
 export default function gameLoop(horodatage){
     let deltatime = horodatage - lastime;
     lastime =horodatage;
-
+   
     ctx.clearRect(0,0,game_width,game_height);
-
     game.update(deltatime)
     game.draw(ctx)
 
